19dd404b5b6838d4baccbfa76a85d754ce9b5bac
J Perez <jperezopt@gmail.com>
2026-08-26 21:15:16 -0500

    Add home page

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 from fastapi import FastAPI
-from database import get_submissions
-from models import Submissions
+from fastapi.templating import Jinja2Templates
+from fastapi.requests import Request
+from fastapi.responses import HTMLResponse
+from database import get_submissions, insert_submission
+from models import Submissions, Submission
 from sqlite3 import Connection, Row
 
 app = FastAPI()
 cx = Connection('app.db')
 cx.row_factory = Row
 
+templates = Jinja2Templates('./templates')
+
+@app.get('/')
+async def home(request: Request) -> HTMLResponse:
+    return templates.TemplateResponse(request, './index.html', context={})
+
+
 @app.get("/submissions")
-async def submissions()->Submissions:
+async def submissions() -> Submissions:
     return get_submissions(cx)
+
+@app.post("/submisson")
+async def submit(submission: Submission) -> Submission:
+    insert_submission(cx, submission)
+    return submission
