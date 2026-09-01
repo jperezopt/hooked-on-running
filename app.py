20a098535f0ffc40b269b100dd90e9c982a36a73
J Perez <jperezopt@gmail.com>
2026-09-01 13:32:02 -0500

    doge

--- app.py
+++ app.py
@@ -6,11 +6,15 @@ from database import get_submissions, insert_submission
 from models import Submissions, Submission
 from sqlite3 import Connection, Row
 
+from fastapi.staticfiles import StaticFiles
+
 app = FastAPI()
 cx = Connection("app.db")
 cx.row_factory = Row
 
 templates = Jinja2Templates("./templates")
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get("/")
@@ -24,11 +28,15 @@ async def home(request: Request) -> HTMLResponse:
 @app.get("/submissions")
 async def submissions(request: Request) -> HTMLResponse:
     submissions = get_submissions(cx)
-    return templates.TemplateResponse(request, './submissions.html', context=submissions.model_dump())
+    return templates.TemplateResponse(
+        request, "./submissions.html", context=submissions.model_dump()
+    )
 
 
 @app.post("/submission")
 async def submission(request: Request, submission: Submission) -> HTMLResponse:
     insert_submission(cx, submission)
     submissions = get_submissions(cx)
-    return templates.TemplateResponse(request, './submissions.html', context=submissions.model_dump())
+    return templates.TemplateResponse(
+        request, "./submissions.html", context=submissions.model_dump()
+    )
